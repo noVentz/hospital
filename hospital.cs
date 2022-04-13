08102b828b0cce8fc483675ba05096bd7ace0a82
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-14 00:10:31 +0300

    Initial commit

---
+++ hospital.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many grandmothers are in the queue?");
+            int timeWaiting = 10;
+            int peoples = Convert.ToInt32(Console.ReadLine()) * timeWaiting;
+            int timeInHours = 60;
+            int waitingInHours = peoples / timeInHours;
+            int waitingInMinutes = peoples % timeInHours;
+            Console.WriteLine("Waiting time in queue {0} hours {1} minutes", waitingInHours, waitingInMinutes);
+        }
+    }
+}
